f896c64738712af67ea137f7f79681adeb593992
Leza Lutonda (Lemol-C) <lemol-c@hotmail.com>
2019-02-20 00:29:56 +0100

    improve useItemCreate

--- src/elements/crud/useItemCreate.ts
+++ src/elements/crud/useItemCreate.ts
@@ -1,36 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { join } from 'lodash';
-import { useMutation } from 'react-apollo-hooks';
-import gql from 'graphql-tag';
+import React, { useState, useEffect } from "react";
+import { join } from "lodash";
+import { useMutation } from "react-apollo-hooks";
+import gql from "graphql-tag";
 
 export type NotificationFn = (msg: string) => void;
 
-export type CallbackOptions = {
-  item?: any,
-  error?: any,
+export interface ErrorCallbackOptions<TInput> {
+  input?: TInput;
+  error: any;
 }
 
-export type CallbackFn = (options: CallbackOptions) => void;
-
-export type Options = {
-  typeName: string,
-  fields: Array<string>,
-  refetchQueries?: Array<string>,
-  errorNotification?: NotificationFn,
-  successNotification?: NotificationFn,
-  onError?: CallbackFn,
-  onSuccess?: CallbackFn,
+export interface SuccessCallbackOptions<TInput, TResult> {
+  input?: TInput;
+  result: TResult;
 }
 
-const makeCreateMutation = ({ createMutationName, fields, typeName, inputTypeName }: any) => gql`
-  mutation Create${typeName}($item: ${inputTypeName}!) {
-    ${createMutationName}(item: $item) {
-      id ${join(fields, ' ')}
+export type ErrorCallbackFn<TInput> = (
+  options: ErrorCallbackOptions<TInput>
+) => void;
+export type SuccessCallbackFn<TInput, TResult> = (
+  options: SuccessCallbackOptions<TInput, TResult>
+) => void;
+
+export interface Options<TInput, TResult> {
+  typeName: string;
+  fields: Array<string>;
+  refetchQueries?: Array<string>;
+  errorNotification?: NotificationFn;
+  successNotification?: NotificationFn;
+  onError?: ErrorCallbackFn<TInput>;
+  onSuccess?: SuccessCallbackFn<TInput, TResult>;
+}
+
+function makeCreateMutation({
+  createMutationName,
+  fields,
+  typeName,
+  inputTypeName
+}: any) {
+  return gql`
+    mutation Create${typeName}($result: ${inputTypeName}!) {
+      ${createMutationName}(result: $result) {
+        id ${join(fields, " ")}
+      }
     }
-  }
-`;
+  `;
+}
 
-const makeOptions = ({ typeName, fields }: any) => {
+function makeOptions({ typeName, fields }: any) {
   const inputTypeName = `${typeName}Input`;
   const createName = `create${typeName}`;
 
@@ -38,93 +55,146 @@ const makeOptions = ({ typeName, fields }: any) => {
     typeName,
     inputTypeName,
     createName,
-    fields,
+    fields
   };
-};
-
-type MakeAfterSaveOptions = {
-  errorNotification?: NotificationFn,
-  successNotification?: NotificationFn,
-  onError?: CallbackFn,
-  onSuccess?: CallbackFn,
 }
 
-const handleAfterSave = ({
-  errorNotification,
-  successNotification,
-  onError,
-  onSuccess,
-}: MakeAfterSaveOptions) => async (item?: any, error?: any) => {
-  if (item) {
-    if (successNotification) {
-      successNotification('Adicionado com sucesso.');
-    }
-
-    if (onSuccess) {
-      onSuccess({ item });
-    }
-  } else if (error) {
-    if (errorNotification) {
-      errorNotification('Erro tentado adicionar novo item.');
-    }
-
-    if (onError) {
-      onError({ item, error });
-    }
-  }
+type MakeSaveSuccessHandlerOptions<TInput, TResult> = {
+  successNotification?: NotificationFn;
+  onSuccess?: SuccessCallbackFn<TInput, TResult>;
+  errorNotification?: NotificationFn;
+  onError?: ErrorCallbackFn<TInput>;
 };
 
-export default ({
+function makeSaveHandler<TInput, TResult>({
+  successNotification,
+  onSuccess,
+  errorNotification,
+  onError
+}: MakeSaveSuccessHandlerOptions<TInput, TResult>) {
+  return async (input?: TInput, result?: TResult, error?: any) => {
+    if (error) {
+      if (errorNotification) {
+        errorNotification("Error.");
+      }
+
+      if (onError) {
+        onError({ input, error });
+      }
+    } else {
+      if (successNotification) {
+        successNotification("Success.");
+      }
+
+      if (onSuccess) {
+        onSuccess({ input, result: result as TResult });
+      }
+    }
+  };
+}
+
+export type UseCreateItemResult<TInput, TResult> = {
+  save: (input: TInput) => void | Promise<void>;
+  saving: boolean;
+  result?: TResult;
+  currentInput?: TInput;
+  error: any;
+};
+export type UseCreateItem<TInput, TResult> = (
+  initialInput?: TInput
+) => UseCreateItemResult<TInput, TResult>;
+
+export default function  makeUseItemCreate<TInput, TResult>({
   typeName,
   fields,
   refetchQueries,
   errorNotification,
   successNotification,
   onError,
-  onSuccess,
-}: Options) => {
+  onSuccess
+}: Options<TInput, TResult>): UseCreateItem<TInput, TResult> {
   const options = makeOptions({ typeName, fields });
   const createMutation = makeCreateMutation(options);
-  const afterSave = handleAfterSave({
+  const onSave = makeSaveHandler({
     successNotification,
-    errorNotification,
     onSuccess,
-    onError,
+    errorNotification,
+    onError
   });
 
-  return (initialValue: any) => {
-    const [item, setItem] = useState(initialValue);
-    const [error, setError] = useState(null);
+  return (initialInput?: TInput): UseCreateItemResult<TInput, TResult> => {
+    const [currentInput, setCurrentInput] = useState<TInput | undefined>(
+      initialInput
+    );
+    const [result, setResult] = useState<TResult | undefined>(undefined);
+    const [error, setError] = useState<any>(null);
     const [saving, setSaving] = useState(false);
+    const [clean, setClean] = useState(true);
 
-    const mutation = useMutation(createMutation, {
+    const mutation = useMutation<TResult>(createMutation, {
       refetchQueries: () => refetchQueries ? refetchQueries : [],
     });
 
-    useEffect(() => {
-      afterSave(item, error);
-    }, [item, error]);
+    // const mutation = ({ variables }: any) =>
+      // new Promise((resolve, reject) => {
+        // setTimeout(() => {
+          // if (variables.item.a === "10") {
+            // return reject(new Error(variables));
+          // }
+//
+          // resolve(variables.item);
+        // }, 0);
+      // });
 
-    const save = async (value: any) => {
+    useEffect(
+      () => {
+        if (!clean) {
+          onSave(currentInput, result, error);
+        }
+      },
+      [result, error]
+    );
+
+    const save = async (input: TInput) => {
       setSaving(true);
+      setClean(false);
+      setCurrentInput(input);
       try {
         const result = await mutation({
           variables: {
-            item: value,
-          },
+            item: input
+          }
         });
 
+        setResult(result.data);
         setError(null);
-        setItem(result);
-        return result;
-      } catch(error) {
-        setError(error);
-        setItem(null);
+      } catch (catchedError) {
+        setResult(undefined);
+        setError(catchedError);
       } finally {
         setSaving(false);
       }
-    }
+    };
 
-    return { save, saving, item, error };
-  }
-};
+    return { save, saving, result, currentInput, error };
+  };
+}
+
+// interface MyInput {
+  // name: string;
+  // code: string;
+// }
+//
+// interface MyResult {
+  // name: string;
+  // code: string;
+// }
+//
+// const useItem = makeUseItemCreate<MyInput, MyResult>({
+  // typeName: 'Pais',
+  // fields: ['name', 'code']
+// });
+//
+// function Novo() {
+  // const { save, saving } = useItem();
+// }
